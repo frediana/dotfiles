f906273b4124f9c626336cdf681b79a068a00466
Adrien Frediani <frediani.adrien@gmail.com>
2015-07-18 00:42:00 +0200

    install: add script install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+ln -sf ~/.dotfiles/zshrc ~/.zshrc
+ln -sf ~/.dotfiles/tig2rc ~/.tigrc
+
+rm -rf ~/.vim/bundle/
+mkdir -p ~/.vim/bundle
+mkdir -p ~/.vim/backup
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+ln -sf ~/.dotfiles/vimrc ~/.vimrc
+vim +PluginInstall +qall
+
+ln -sf ~/.dotfiles/gitconfig ~/.gitconfig
